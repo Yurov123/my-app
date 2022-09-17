e4b1060fbb748eb5e8b8fca18c7e46fee50a3c23
Konstantin <constantin8271@gmail.com>
2022-09-17 13:20:43 +0300

    init project

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CountersList from "./components/countersList";
+
+const App = () => {
+  return (
+    <div className="App">
+      <CountersList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import reactDom from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./app";
+
+reactDom.render(<App/>, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
