b1d12ba3f958e305037ec4362b6780cb697681b1
Konstantin <constantin8271@gmail.com>
2022-09-19 18:47:37 +0300

    fix code 2

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,15 +13,21 @@ const Counter = (props) => {
         return classes;
     };
 
+    const handleIncrement = () => {
+        props.onIncrement(props.id);
+    };
+    const handleDecrement = () => {
+        props.onDecrement(props.id);
+    };
 
     return (
         <div className='container'>
             <span>{props.name}</span>
             <span className={getBageClasses()}>{formatValue()}</span>
             <button className="btn btn-primary btn-sm m-2"
-                onClick={() => props.onIncrement(props.id)}>+</button>
+                onClick={handleIncrement}>+</button>
             <button className="btn btn-primary btn-sm m-2"
-                onClick={() => props.onDecrement(props.id)}>-</button>
+                onClick={handleDecrement}>-</button>
             <button className='btn btn-danger btn-sm m-2'
                 onClick={() => props.onDelete(props.id)}>Delete</button>
 
